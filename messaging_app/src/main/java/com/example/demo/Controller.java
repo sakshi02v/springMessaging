a50dd16f90055f1418514420831387ea5543777c
Sakshi Verma <sakshi02v@gmail.com>
2025-02-22 11:53:44 +0530

    [Sakshi Verma]Add. Use PUT Request Method and pass first name as Path Variable and last name as Query Parameter

--- messaging_app/src/main/java/com/example/demo/Controller.java
+++ messaging_app/src/main/java/com/example/demo/Controller.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 	@RestController
 	public class Controller {
+		
 	    @GetMapping("/hello")
 	    public String hello(){
 	        return "Hello from Bridgelabz ";
@@ -24,9 +27,14 @@ import org.springframework.web.bind.annotation.RestController;
 	        return "Hello " + name + " from BridgeLabz";
 	    }
 	    
-	    @PostMapping("/hello/post")
-	    public String postHello(@RequestBody User user) {
+	    @PostMapping("/post")
+	    public String postHello(@RequestBody UserDTO user) {
 	        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+	    }
+	    
+	    @PutMapping("/hello/put/{firstName}")
+	    public String putHello(@PathVariable String firstName, @RequestParam String lastName) {
+	        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
 	    }
 
 }
