80e1fdafefaa3775237c368a2807a6615738085b
Sakshi Verma <sakshi02v@gmail.com>
2025-02-22 10:54:46 +0530

    [Sakshi Verma] Add. Make REST Call to show Hello  Mark from BridgeLabz

--- messaging_app/src/main/java/com/example/demo/Controller.java
+++ messaging_app/src/main/java/com/example/demo/Controller.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 	
-	import org.springframework.web.bind.annotation.GetMapping;
-	import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 	@RestController
 	public class Controller {
 	    @GetMapping("/hello")
 	    public String hello(){
 	        return "Hello from Bridgelabz ";
+	    }
+	    
+	    @GetMapping("/query")
+	    public String getHello(@RequestParam String name) {
+	        return "Hello " + name + " from BridgeLabz";
 	    }
 
 }
